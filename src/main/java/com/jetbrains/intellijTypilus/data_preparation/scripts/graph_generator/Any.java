fb4e24f19b94f5b15e683a8cda3c33e1170414a0
Yalman Ahadi <zcabyah@ucl.ac.uk>
2020-12-10 01:16:04 +0000

    Child edges adding for simple assignment statements

---
+++ src/main/java/com/jetbrains/intellijTypilus/data_preparation/scripts/graph_generator/Any.java
@@ -0,0 +1,4 @@
+package com.jetbrains.intellijTypilus.data_preparation.scripts.graph_generator;
+
+public class Any {
+}
--- src/main/java/com/jetbrains/intellijTypilus/data_preparation/scripts/graph_generator/GraphGenerator.java
+++ src/main/java/com/jetbrains/intellijTypilus/data_preparation/scripts/graph_generator/GraphGenerator.java
@@ -5,21 +5,31 @@ import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiRecursiveElementVisitor;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl;
+import com.intellij.psi.tree.IElementType;
 import com.jetbrains.python.PyTokenTypes;
 import com.jetbrains.python.psi.PyAssignmentStatement;
 import com.jetbrains.python.psi.PyFile;
+import com.jetbrains.python.psi.PyTargetExpression;
+import com.jetbrains.python.psi.PyTypedElement;
+import com.jetbrains.python.psi.types.TypeEvalContext;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 
 import java.util.*;
 
 public class GraphGenerator {
-    HashMap<String, Object> graph = new HashMap<String, Object>();
-    HashMap<EdgeType, HashMap<Integer, Set<Integer>>> edges;
+    LinkedHashMap<String, Object> graph = new LinkedHashMap<String, Object>();
+    //LinkedHashMap<EdgeType, LinkedHashMap<Integer, Set<Integer>>> edges;
     ArrayList<Object> idToNode = new ArrayList<>();
-    public HashMap<Object, Integer> nodeToId = new HashMap<>();
-    PsiFile psiFile = null;
-    PsiElement currentParentNode = null;
+    public LinkedHashMap<Object, Integer> nodeToId = new LinkedHashMap<>();
+    public PsiFile psiFile = null;
+    public PsiElement currentParentNode = null;
     public GraphGenerator(AnActionEvent e){
         this.psiFile = e.getData(LangDataKeys.PSI_FILE);
 
@@ -36,36 +46,35 @@ public class GraphGenerator {
     }
 
     void makeGraphStructure(){
-        HashMap<String, ArrayList<Integer>> edgeElement = new HashMap<String, ArrayList<Integer>>();
+        LinkedHashMap<String, ArrayList<Integer>> edgeElement = new LinkedHashMap<>();
 
         this.graph.put("nodes", new ArrayList<String>());
-        this.graph.put("edges", new HashMap<String, HashMap<String, ArrayList<Integer>>>());
-        ((HashMap<String, HashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put("CHILD", edgeElement);
-        ((HashMap<String, HashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put("NEXT", edgeElement);
-        ((HashMap<String, HashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put("COMPUTED_FROM" , edgeElement);
-        ((HashMap<String, HashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put("RETURNS_TO", edgeElement);
-        ((HashMap<String, HashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put("OCCURRENCE_OF", edgeElement);
-        ((HashMap<String, HashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put("SUBTOKEN_OF", edgeElement);
+        this.graph.put("edges", new LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>());
+        ((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put(EdgeType.CHILD, edgeElement);
+        ((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put(EdgeType.NEXT, edgeElement);
+        ((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put(EdgeType.COMPUTED_FROM , edgeElement);
+        ((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put(EdgeType.RETURNS_TO, edgeElement);
+        ((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put(EdgeType.OCCURRENCE_OF, edgeElement);
+        ((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put(EdgeType.SUBTOKEN_OF, edgeElement);
         this.graph.put("token-sequence", new ArrayList<Integer>());
-        this.graph.put("supernodes", new HashMap<String, HashMap<String, Object>>());
+        this.graph.put("supernodes", new LinkedHashMap<String, LinkedHashMap<String, Object>>());
 
     }
     public String toString(){
         return graph.toString();
     }
-
-    public static void main(String[] args) {
-        HashMap<Object, Integer> map = new HashMap<>();
-        map.put("abc", 123);
-        ArrayList<Integer> list = new ArrayList<>();
-        map.put(list, 234);
-        System.out.println(map.toString());
-        GraphGenerator graphGenerator = new GraphGenerator();
-        graphGenerator.build();
-        graphGenerator.nodeID(123);
-        System.out.println(graphGenerator.nodeToId);
-
-    }
+//
+//    public static void main(String[] args) {
+//        LinkedHashMap<Object, Integer> map = new LinkedHashMap<>();
+//        map.put("abc", 123);
+//        ArrayList<Integer> list = new ArrayList<>();
+//        map.put(list, 234);
+//        System.out.println(map.toString());
+//        GraphGenerator graphGenerator = new GraphGenerator();
+//        graphGenerator.build();
+//        System.out.println(graphGenerator.nodeToId);
+//
+//    }
 
     String nodeToLabel(Object node) throws ClassNotFoundException {
         if (node instanceof String){
@@ -78,102 +87,140 @@ public class GraphGenerator {
     }
 
 
-    public int nodeID(Object node){
-        if (this.nodeToId.get(node) == null){
-            System.out.println("herer");
-            if(node instanceof TokenNode) {
-                int idx = this.nodeToId.size();
-                assert (this.idToNode.size() == this.nodeToId.size());
-                this.nodeToId.put(node, idx);
-                this.idToNode.add(node);
-            }
-            else if (node instanceof PsiElement) {
-                nodeID((PsiElement) node);
-            }
-        }
-        return this.nodeToId.get(node);
-    }
+//    public int nodeID(Object node){
+//        if (this.nodeToId.get(node) == null){
+//            System.out.println("herer");
+//            if(node instanceof TokenNode) {
+//                int idx = this.nodeToId.size();
+//                assert (this.idToNode.size() == this.nodeToId.size());
+//                this.nodeToId.put((TokenNode) node, idx);
+//                this.idToNode.add((TokenNode) node);
+//            }
+//            else if (node instanceof PsiElement) {
+//                nodeID((PsiElement) node);
+//            }
+//        }
+//        return this.nodeToId.get(node);
+//    }
 
     int nodeID(PsiElement node){
+        //assert needed maybe
+
+        if (this.nodeToId.get(node) == null){
             int idx = this.nodeToId.size();
-            assert (this.idToNode.size() == this.nodeToId.size());
+            assert (idToNode.size() == nodeToId.size());
             this.nodeToId.put(node, idx);
             this.idToNode.add(node);
             return idx;
+        }
+        return this.nodeToId.get(node);
+//        if (this.nodeToId.get(node) != null){
+//        int idx = this.nodeToId.size();
+//        if (this.idToNode.size() == this.nodeToId.size()) {
+//                this.nodeToId.put(node, idx);
+//                this.idToNode.add(node);
+//            }
+//        return idx;
     }
 
-    void addEdge(TokenNode fromNode, TokenNode toNode, EdgeType edgeType) { //need to add edges part
-        int fromNodeIdx = this.nodeID(fromNode);
-        int toNodeIdx = this.nodeID(toNode);
-        System.out.printf("adding edge type: %s", edgeType);
-        this.edges.get(edgeType).get(fromNodeIdx).add(toNodeIdx);
-    }
+//    void addEdge(TokenNode fromNode, TokenNode toNode, EdgeType edgeType) { //need to add edges part
+//        int fromNodeIdx = this.nodeID(fromNode);
+//        int toNodeIdx = this.nodeID(toNode);
+//        System.out.printf("adding edge type: %s", edgeType);
+//        this.edges.get(edgeType).get(fromNodeIdx).add(toNodeIdx);
+//    }
 
     void addEdge(PsiElement fromNode, PsiElement toNode, EdgeType edgeType) { //need to add edges part
         int fromNodeIdx = this.nodeID(fromNode);
         int toNodeIdx = this.nodeID(toNode);
-        System.out.printf("adding edge type: %s", edgeType);
-        this.edges.get(edgeType).get(fromNodeIdx).add(toNodeIdx);
-        System.out.println(this.edges);
+        System.out.println(((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).get(EdgeType.CHILD));
+        if (((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).get(edgeType).containsKey(Integer.toString(fromNodeIdx))){
+            ((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).get(edgeType).get(Integer.toString(fromNodeIdx)).add(toNodeIdx);
+        }
+        else{
+            LinkedHashMap<String, ArrayList<Integer>> newMap  = new LinkedHashMap<>();
+            newMap.put(Integer.toString(fromNodeIdx), new ArrayList<Integer>(Arrays.asList(toNodeIdx)));
+            ((LinkedHashMap<EdgeType, LinkedHashMap<String, ArrayList<Integer>>>) this.graph.get("edges")).put(edgeType, newMap);
+        }
+        System.out.println(this.graph);
     }
 
-    void addEdge(PsiElement fromNode, TokenNode toNode, EdgeType edgeType) { //need to add edges part
-        int fromNodeIdx = this.nodeID(fromNode);
-        int toNodeIdx = this.nodeID(toNode);
-        System.out.printf("adding edge type: %s", edgeType);
-        this.edges.get(edgeType).get(fromNodeIdx).add(toNodeIdx);
-        System.out.println(this.edges);
-    }
-    void addEdge(TokenNode fromNode, PsiElement toNode, EdgeType edgeType) { //need to add edges part
-        int fromNodeIdx = this.nodeID(fromNode);
-        int toNodeIdx = this.nodeID(toNode);
-        System.out.printf("adding edge type: %s", edgeType);
-        this.edges.get(edgeType).get(fromNodeIdx).add(toNodeIdx);
-        System.out.println(this.edges);
-    }
+//    void addEdge(PsiElement fromNode, TokenNode toNode, EdgeType edgeType) { //need to add edges part
+//        int fromNodeIdx = this.nodeID(fromNode);
+//        int toNodeIdx = this.nodeID(toNode);
+//        System.out.printf("adding edge type: %s", edgeType);
+//        this.edges.get(edgeType).get(fromNodeIdx).add(toNodeIdx);
+//        System.out.println(this.edges);
+//    }
+//    void addEdge(TokenNode fromNode, PsiElement toNode, EdgeType edgeType) { //need to add edges part
+//        int fromNodeIdx = this.nodeID(fromNode);
+//        int toNodeIdx = this.nodeID(toNode);
+//        System.out.printf("adding edge type: %s", edgeType);
+//        this.edges.get(edgeType).get(fromNodeIdx).add(toNodeIdx);
+//        System.out.println(this.edges);
+//    }
 
 
-    public void visit(PsiElement node){
-        System.out.println(node.getFirstChild().getNode().getPsi().getText());
-        PsiRecursiveElementVisitor visitor = new PsiRecursiveElementVisitor() {
-            @Override
-            public void visitElement(@NotNull PsiElement element) {
-                super.visitElement(element);
-            }
-        };
-        psiFile.accept(visitor);
-        this.currentParentNode = psiFile.getFirstChild();
-        if(this.currentParentNode != null){
-            if(this.nodeToId.containsKey(this.currentParentNode) || (this.currentParentNode instanceof PyFile)) {
+    public void visit(PsiElement node) {
+//        System.out.println(node.getFirstChild().getNode().getPsi().getText());
+        if (node instanceof PsiWhiteSpaceImpl){
+            return;
+        }
+
+        if (this.currentParentNode != null) {
+            System.out.println("in");
+            System.out.println(this.currentParentNode.getNode().getElementType().getClass().getSimpleName());
+            if (this.nodeToId.containsKey(this.currentParentNode) || (this.currentParentNode.getNode().getElementType().getClass().getSimpleName().equals("PyFileElementType"))) {
                 System.out.println("here");
                 this.addEdge(this.currentParentNode, node, EdgeType.CHILD);
             }
         }
         PsiElement parent = this.currentParentNode;
         this.currentParentNode = node;
-        try{
-            System.out.printf("context %s", node.getFirstChild().getNode()); /////////////
-            //visit_AnnAssign(node.);
+        try {
+//            System.out.printf("context %s", node.getClass().getSimpleName().replace("Impl", "")); /////////////
 
-        }finally {
+            String methodName = "visit" + node.getClass().getSimpleName().replace("Impl", "");
 
+            Class<?> c = Class.forName("com.jetbrains.intellijTypilus.data_preparation.scripts.graph_generator.GraphGenerator");
+            Method method = c.getDeclaredMethod(methodName, PsiElement.class);
+            method.invoke(this, node);
+
+
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        } finally {
+            this.currentParentNode = parent;
         }
     }
-    void visitModule(PsiElement file){
 
-    }
-    void visitAnnAssign(PyAssignmentStatement node){
-        if (node.getAssignedValue() != null){
-            System.out.println("visitied the assinment");
-            this.visit(node.getAssignedValue());
+    void visitPyFile(PsiElement file){
+        for (PsiElement element : file.getChildren()){
+            visit(element);
         }
+    }
+    void visitPyAssignmentStatement(PsiElement node) {
+//        for(PsiElement element : node.getChildren()){
+//            System.out.println("herer");
+//            System.out.println(PyTargetExpression.class.getSimpleName());
+//            System.out.println(element.getClass().getSimpleName());
+//            if (element.getClass().getSimpleName().equals(PyTargetExpression.class.toGenericString())){
+//
+//            }
+//        }
+//        if (node.getAssignedValue() != null) {
+//            System.out.println("visitied the assinment");
+//            this.visit(statement.getAssignedValue());
+//        }
+
+
 
     }
 
 
 
     void add_terminal(TokenNode tokenNode){
-        this.addEdge(this.currentParentNode, tokenNode, EdgeType.CHILD);
+        //this.addEdge(this.currentParentNode, tokenNode, EdgeType.CHILD);
     }
 
 
